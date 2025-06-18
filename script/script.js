5bbfa362d304c6581dc211b224f980b522fa6cc4
Gabriel10191 <silva.gabriel@estudante.uffs.edu.br>
2025-06-18 17:16:14 -0300

    tentativa de java

---
+++ script/script.js
@@ -0,0 +1,19 @@
+
+document.querySelector('form[role="search"]').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const pesquisa = document.querySelector('input[type="search"]').value.toLowerCase();
+    const resultadosDiv = document.querySelectorAll('.card');
+    let encontrou = false;
+    resultadosDiv.forEach(card => {
+        const titulo = card.querySelector('.card-title').textContent.toLowerCase()
+        if (titulo.includes(pesquisa)){
+            card.style.display = 'block';
+            encontrou = true;
+        } else {
+            card.style.display = 'none';
+        }
+    });
+    if (!encontrou) {
+        alert('nenhum produto encontrado.');
+    }
+});
